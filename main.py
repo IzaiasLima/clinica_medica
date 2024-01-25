239d3d4d9759da7e107ce3153bcc6a4d6795f840
Izaias Lima <izaiasmlima@gmail.com>
2024-01-25 12:13:20 -0300

    Adicionar redireciomanto para pagina de login

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 
 app = FastAPI()
 app.mount("/app", StaticFiles(directory="static", html="True"), name="static")
@@ -11,6 +11,13 @@ async def root():
     return "/app/login.html"
 
 
-@app.get("/api")
+@app.get("/api", response_class=HTMLResponse)
 async def api():
-    return {"dados1": "Lista de medicos", "dados2": "Lista de pacientes"}
+    return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta http-equiv="refresh" content="0; URL='./app/login.html'">
+        </head>
+        </html>
+    """
