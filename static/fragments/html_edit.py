646a72accaf2bbb01a2e1053b1eea1f1dfc0c128
Izaias Lima <izaiasmlima@gmail.com>
2024-03-19 15:46:43 -0300

    Alterar formulario de edicao de medicos e pacientes

--- static/fragments/html_edit.py
+++ static/fragments/html_edit.py
@@ -1,44 +1,47 @@
 def paciente_html(dados):
     html = f"""
-        <tr trigger="cancel" class="editing">
-            <td><input name="nome" value="{dados['nome']}"></td>
-            <td><input name="email" value="{dados['email']}"></td>
-            <td><input name="telefone" onkeyup="phoneMask(this)" value="{dados['telefone']}"></td>
-            <td>
-                <select name="status">
-                    <option value="" selected disabled hidden>Selecione</option>
-                    <option value="aguardando">Aguardando</option>
-                    <option value="em atendimento">Em atendimento</option>
-                    <option value="atendido">Atendido</option>
-                    <option value="internado">Internado</option>
-                    <option value="liberado">Liberado</option>
-                </select>
-            </td>
-            <td class="icon">
-                <div>
-                    <a hx-get="/api/pacientes"
-                        title="Cancelar a alteração"
-                        hx-swap="outerHTML" 
-                        hx-target="closest table">
-                        <i class="material-symbols-outlined small-icon">undo</i>
-                    </a>
-                    <a hx-trigger="click" 
-                        hx-include="closest tr"
-                        hx-put="/api/pacientes/{dados['id']}" 
-                        title="Salvar"
-                        hx-swap="outerHTML" 
-                        hx-target="closest table"
-                        <i class="material-symbols-outlined small-icon">save</i>
-                    </a>
-                </div>
-            </td>
-        </tr>
+        <table class="form">
+            <tr trigger="cancel" class="editing">
+                <td><input name="nome" value="{dados['nome']}"></td>
+                <td><input name="email" value="{dados['email']}"></td>
+                <td><input name="telefone" onkeyup="phoneMask(this)" value="{dados['telefone']}"></td>
+                <td>
+                    <select name="status">
+                        <option value="" selected disabled hidden>Selecione</option>
+                        <option value="aguardando">Aguardando</option>
+                        <option value="em atendimento">Em atendimento</option>
+                        <option value="atendido">Atendido</option>
+                        <option value="internado">Internado</option>
+                        <option value="liberado">Liberado</option>
+                    </select>
+                </td>
+                <td class="icon">
+                    <div>
+                        <a class="button secondary" hx-get="/api/pacientes"
+                            title="Cancelar a alteração"
+                            hx-swap="outerHTML" 
+                            hx-target="closest table">
+                            Cancelar
+                        </a>
+                        <a class="button" hx-trigger="click" 
+                            hx-include="closest tr"
+                            hx-put="/api/pacientes/{dados['id']}" 
+                            title="Salvar"
+                            hx-swap="outerHTML" 
+                            hx-target="closest table">
+                            Salvar
+                        </a>
+                    </div>
+                </td>
+            </tr>
+        </table>
         """
     return html
 
 
 def medico_html(dados):
     html = f"""
+    <table class="form">
         <tr trigger="cancel" class="editing">
             <td><input name="nome" value="{dados['nome']}"></td>
             <td><input name="crm" value="{dados['crm']}"></td>
@@ -63,19 +66,19 @@ def medico_html(dados):
 
             <td class="icon">
                 <div>
-                    <a hx-get="/api/medicos"
+                    <a class="button secondary" hx-get="/api/medicos"
                         title="Cancelar a alteração"
                         hx-swap="outerHTML" 
                         hx-target="closest table">
-                        <i class="material-symbols-outlined small-icon">undo</i>
+                        Cancelar
                     </a>
-                    <a hx-trigger="click" 
+                    <a class="button" hx-trigger="click" 
                         hx-include="closest tr"
                         hx-put="/api/medicos/{dados['id']}" 
                         title="Salvar"
                         hx-target="closest table"
                         hx-swap="outerHTML">
-                        <i class="material-symbols-outlined small-icon">save</i>
+                        Salvar
                     </a>
                 </div>
             </td>
